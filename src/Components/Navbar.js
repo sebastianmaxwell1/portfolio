df9e0a37f92c43d750f46c0fd1a402994410e7f1
Sebastian <maxwellsebas@gmail.com>
2021-07-08 13:58:05 -0400

    Added links and light style to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import logo from "../logo.png";
+//FONT AWESOME//
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 
 const Navbar = () => {
     return (
@@ -10,7 +14,7 @@ const Navbar = () => {
                 
   <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <FontAwesomeIcon icon={faBars} style={{color:"green"}}/>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -19,7 +23,16 @@ const Navbar = () => {
         <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+        <a className="nav-link" href="#">About Me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Services</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact</a>
       </li>
 
       </ul>
